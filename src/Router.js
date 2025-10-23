c684b4c8f7db3f9422f476e0bc8695237de66b8b
Alejandro4092 <acobomarcos@gmail.com>
2025-10-23 12:47:28 +0200

    Routes

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import axios
+import axios from 'axios'
+import { BrowserRouter, Routes } from 'react-router-dom'
+import MenuRutas from './components/MenuRutas'
 
 export default class Router extends Component {
   render() {
