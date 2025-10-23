698eb8d263a20f2bd83513e9576da7f5082c43df
Alejandro4092 <acobomarcos@gmail.com>
2025-10-23 12:43:32 +0200

    Router y global

---
+++ src/Router.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import axios
+
+export default class Router extends Component {
+  render() {
+    return (
+     <BrowserRouter>
+        <MenuRutas />
+
+        <Routes>
+          
+        </Routes>
+      </BrowserRouter>
+    )
+  }
+}
